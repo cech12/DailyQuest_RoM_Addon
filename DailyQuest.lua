f011371da6c268afc69030f24da826b16ebc7aee
cech12 <cech12@gmail.com>
2012-04-05 17:51:14 +0000

    - Version 1.1.0 fertig - Einstellungen der Farbe, Schriftgröße, Fixierung und der Anzeige der Textzeile ist nun möglich

--- DailyQuest.lua
+++ DailyQuest.lua
@@ -16,7 +16,11 @@ function g_DailyQuestConfig_Init()
 	if (not g_DailyQuestConfig) then g_DailyQuestConfig = {}; end
 	if (g_DailyQuestConfig.Show == nil) then g_DailyQuestConfig.Show = true;	end
 	if (g_DailyQuestConfig.Lock == nil) then	g_DailyQuestConfig.Lock = false; end
-	--if (g_DailyQuestConfig.Color == nil) then g_DailyQuestConfig.Color = true; end
+	if (g_DailyQuestConfig.FontSize == nil) then g_DailyQuestConfig.FontSize = 12; end
+	if (g_DailyQuestConfig.r == nil) then g_DailyQuestConfig.r = 1; end
+	if (g_DailyQuestConfig.g == nil) then g_DailyQuestConfig.g = 1; end
+	if (g_DailyQuestConfig.b == nil) then g_DailyQuestConfig.b = 1; end
+	if (g_DailyQuestConfig.a == nil) then g_DailyQuestConfig.a = 1; end
 end;
 
 --------------------------------------------------------------------------------------
@@ -34,15 +38,17 @@ function DailyQuestButton_OnMouseUp()
 	UIPanelAnchorFrame_StopMoving();
 end
 
+function DailyQuestSettings_OpenClose()
+  if (DailyQuestSettings:IsVisible() == false) then
+    ShowUIPanel(DailyQuestSettings);
+  else
+    HideUIPanel(DailyQuestSettings);
+  end
+end
+
 function DailyQuestButton_OnClick(this,key)
 	if (key == "LBUTTON") then
-		if (DailyQuestSettings:IsVisible() == false) then
-			--ShowUIPanel(GameConfigFrame); -- hier wird das normale Interface-Menü aufgerufen
-			--OnClick_GCF_Tab(6);           -- TODO: neues Menü mit Einstellungsmoeglichkeiten (Farbe, Fixierung, Anzeigen)
-      ShowUIPanel(DailyQuestSettings);
-		else
-			HideUIPanel(DailyQuestSettings);
-		end
+    DailyQuestSettings_OpenClose();
 	end
 end
 
@@ -62,15 +68,16 @@ function DailyQuest_OnLoad(this)
 	--this:RegisterEvent("QUEST_COMPLETE");
   
   SLASH_DQ1 = "/dq";
-  SLASH_DQ2 = "/tq";
   SLASH_DQREFRESH1 = "/dqr";
-  SLASH_DQREFRESH2 = "/tqa";
+  SLASH_DQSETTINGS1 = "/dqs";
   
   SlashCmdList["DQ"] = dq_slash;
   SlashCmdList["DQREFRESH"] = DailyQuestRefresh;
+  SlashCmdList["DQSETTINGS"] = DailyQuestSettings_OpenClose;
   
+  DEFAULT_CHAT_FRAME:AddMessage("DailyQuest v1.1.0 loaded"); -- wird nach DailyQuestRefresh nicht angezeigt
   DailyQuestRefresh();
-  DEFAULT_CHAT_FRAME:AddMessage("DailyQuest v1.1.0 loaded");
+  DailyQuestShowHide();
 end
 
 function dq_slash()
@@ -78,12 +85,25 @@ function dq_slash()
 end
 
 function DailyQuestRefresh()
-  DailyQuestText:SetText(getDailyQuestText());
+  if (g_DailyQuestConfig) then
+    DailyQuestText:SetText(""); --SetFontSize reagiert nur bei einer Textaenderung
+    DailyQuestText:SetFontSize(g_DailyQuestConfig.FontSize);
+    DailyQuestText:SetColor(g_DailyQuestConfig.r, g_DailyQuestConfig.g, g_DailyQuestConfig.b);
+    DailyQuestText:SetAlpha(g_DailyQuestConfig.a);
+    DailyQuestText:SetText(getDailyQuestText());
+  end
 end
 
---function DailyQuestSetColor()
---  DailyQuestText:SetTextColor(g_DailyQuestConfig.Color);
---end
+function DailyQuestShowHide()
+  if (g_DailyQuestConfig.Show) then
+    ShowUIPanel(DailyQuestAnchorFrame);
+    ShowUIPanel(DailyQuest);
+  else
+    HideUIPanel(DailyQuestAnchorFrame);
+    HideUIPanel(DailyQuest);
+    DEFAULT_CHAT_FRAME:AddMessage("DailyQuest: /dqs to open DailyQuest Settings"); 
+  end
+end
 
 -- waehrend "QUEST_COMPLETE" ist Daily_count() noch nicht aktualisiert. Dies erfolgt erst nach dem zweiten mal "RESET_QUESTTRACK"
 function DailyQuest_OnEvent(event)
@@ -96,6 +116,109 @@ function DailyQuest_OnEvent(event)
   end
 end
 
---function DailyQuest_OnUpdate()
---  DailyQuestText:SetText(_glossary_00978 .. ": " .. dailyQuestCount .. "/" .. dailyQuestsPerDay, 1, 1, 1);
---end
+--------------------------------------------------------------------------------
+--SETTINGS
+
+function DailyQuestSettings_OnLoad(this)
+  DailyQuestSettingsHeadline:SetText("DailyQuest " .. QUEST_TRACK_TOOLTIP_SETTING);
+
+	DailyQuestSetting_FontSize:SetValueStepMode("FLOAT");
+	DailyQuestSetting_FontSize:SetMinMaxValues(8, 20);
+	DailyQuestSetting_FontSize:SetValue(1.0);
+end
+
+function DailyQuestSettings_OnShow(this)
+	DailyQuestSetting_Show:SetChecked(g_DailyQuestConfig.Show);
+	DailyQuestSetting_Lock:SetChecked(g_DailyQuestConfig.Lock);
+  
+	DailyQuestSetting_FontSize:SetValue(g_DailyQuestConfig.FontSize);
+
+	g_OldOptions = {};
+	for k,v in pairs(g_DailyQuestConfig) do 
+		g_OldOptions[k] = v;
+	end
+end
+
+--Settings
+
+function DailyQuestSetting_Show_OnClick(this)
+	g_DailyQuestConfig.Show = this:IsChecked();
+  DailyQuestShowHide();
+end
+
+function DailyQuestSetting_Lock_OnClick(this)
+	g_DailyQuestConfig.Lock = this:IsChecked();
+  --DailyQuestRefresh();
+end
+
+function DailyQuestChangeColor()
+	g_DailyQuestConfig.a = ColorPickerFrame.a ;
+	g_DailyQuestConfig.r = ColorPickerFrame.r ;
+	g_DailyQuestConfig.g = ColorPickerFrame.g ;
+	g_DailyQuestConfig.b = ColorPickerFrame.b ;
+  DailyQuestRefresh();
+end
+
+function DailyQuestSetting_Color_OnClick(this)
+	local info = {};
+	info.alphaMode = 1;  -- alpha Slider
+	info.r	= g_DailyQuestConfig.r;   -- default 1
+	info.g	= g_DailyQuestConfig.g;   -- default 1
+	info.b	= g_DailyQuestConfig.b;  -- default 1
+	info.a	= g_DailyQuestConfig.a;   -- default 1
+	info.brightnessUp = 1; -- default 1
+	info.brightnessDown = 0; -- default 0
+	info.callbackFuncOkay = DailyQuestChangeColor;
+	info.callbackFuncUpdate = DailyQuestChangeColor;
+	info.callbackFuncCancel = DailyQuestChangeColor;
+
+	OpenColorPickerFrameEx(info);
+end
+
+function DailyQuestSetting_FontSize_OnValueChanged()
+	Font_OnValueChanged  = DailyQuestSetting_FontSize:GetValue();
+	Font_OnValueChanged = math.floor(Font_OnValueChanged);
+  
+	DailyQuestSetting_FontSize_Size:SetText("");
+	DailyQuestSetting_FontSize_Size:SetText(Font_OnValueChanged);
+
+	if (g_DailyQuestConfig) then
+		g_DailyQuestConfig.FontSize = Font_OnValueChanged;
+    DailyQuestRefresh();
+	end
+end
+
+--Buttons
+
+function DailyQuestSettings_Default_OnClick()
+	g_DailyQuestConfig = nil;
+	g_DailyQuestConfig_Init();
+  
+  DailyQuestRefresh();
+  DailyQuestShowHide();
+	DailyQuestSettings_OnShow(DailyQuestSettings);
+end
+
+function DailyQuestSettings_Apply_OnClick()
+	if (g_DailyQuestConfig) then
+		for k,v in pairs(g_DailyQuestConfig) do 
+			g_OldOptions[k] = v;
+		end
+	end
+end
+
+function DailyQuestSettings_Cancel_OnClick(this)
+	if (g_OldOptions) then
+		for k,v in pairs(g_OldOptions) do 
+			g_DailyQuestConfig[k] = v;
+		end
+	end
+  DailyQuestRefresh();
+  DailyQuestShowHide();
+	HideUIPanel(DailyQuestSettings);
+end
+
+function DailyQuestSettings_OK_OnClick()
+	g_OldOptions = nil;	
+	HideUIPanel(DailyQuestSettings);
+end
